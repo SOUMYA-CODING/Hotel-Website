dcbdb1be42baf44394e7bffdc917a4c5ce998102
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-07-16 10:34:59 +0530

    confirm

--- Script/authentication.js
+++ Script/authentication.js
@@ -1,46 +1,38 @@
 //For login page validation
 function loginvalidationFun() {
-
     let error = false;
 
     //Get the data
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    //Validation for email
-    let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if(email === ""){
-        document.getElementById("login_email_error").innerHTML = "Field can't be empty";
-        error = true;
-    }else if(!email.match(mailFormat)){
-        document.getElementById("login_email_error").innerHTML = "Please enter correct email address";
-        error = true;
-    }else{
-        document.getElementById("login_email_error").innerHTML = "";
-        error = false;
+    //Call the functions
+    emailValidation(email);
+    passwordValidation(password);
+
+    if (error) {
+        return true;
+    }
+    else {
+        return false;
     }
 
-    //Validation for password
-    if(password === ""){
-        document.getElementById("password_email_error").innerHTML = "Field can't be empty";
-        error = false;
-    }else if(password <= 8){
-        document.getElementById("password_email_error").innerHTML = "Password is too small should be altleast 8 digit";
-        error = false;
-    }else if(password.match(/[a-z]/g)){
-        document.getElementById("password_email_error").innerHTML = "Password must contain a lowercase letter";
-        error = false;
-    }else if(password.match(/[A-Z]/g)){
-        document.getElementById("password_email_error").innerHTML = "Password must contain a uppercase letter";
-        error = false;
-    }else if(password.match(/[0-9]/g)){
-        document.getElementById("password_email_error").innerHTML = "Password must contain a number";
-        error = false;
-    }else{
-        document.getElementById("password_email_error").innerHTML = "";
-        error = false;
-    }
+}
 
+
+//For Registration page validation
+function registrationvalidationFun() {
+    let error = false;
+
+    //Get the data
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirm_password = document.getElementById("confirm_password").value;
+
+    //Call the functions
+    emailValidation(email);
+    passwordValidation(password);
+    confirmpasswordValidation(confirm_password);
 
     //Error
     if (error) {
@@ -49,5 +41,52 @@ function loginvalidationFun() {
     else {
         return true;
     }
+}
 
+//Email
+function emailValidation(email) {
+    let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email === "") {
+        document.getElementById("login_email_error").innerHTML = "Field can't be empty";
+        return false;
+    } else if (!email.match(mailFormat)) {
+        document.getElementById("login_email_error").innerHTML = "Please enter correct email address";
+        return false;
+    } else {
+        document.getElementById("login_email_error").innerHTML = "";
+        return true;
+    }
+}
+
+function passwordValidation(password) {
+    if (password === "") {
+        document.getElementById("reg_password_error").innerHTML = "Field can't be empty";
+        error = false;
+    } else if (password <= 8) {
+        document.getElementById("reg_password_error").innerHTML = "Password is too small should be altleast 8 digit";
+        error = false;
+    } else if (password.match(/[a-z]/g)) {
+        document.getElementById("reg_password_error").innerHTML = "Password must contain a lowercase letter";
+        error = false;
+    } else if (password.match(/[A-Z]/g)) {
+        document.getElementById("reg_password_error").innerHTML = "Password must contain a uppercase letter";
+        error = false;
+    } else if (password.match(/[0-9]/g)) {
+        document.getElementById("reg_password_error").innerHTML = "Password must contain a number";
+        error = false;
+    } else {
+        document.getElementById("reg_password_error").innerHTML = "";
+        error = true;
+    }
+}
+
+function confirmpasswordValidation(confirm_password) {
+    //Validation for password
+    if (confirm_password != password) {
+        document.getElementById("reg_confirm_password_error").innerHTML = "Field can't be empty";
+        error = false;
+    } else {
+        document.getElementById("reg_confirm_password_error").innerHTML = "Field can't be empty";
+        error = true;
+    }
 }
