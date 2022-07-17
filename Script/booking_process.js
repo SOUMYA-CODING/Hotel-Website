c7f171d13ca2830a30191c07edae4efec45632c1
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-07-17 22:45:04 +0530

    confirm

--- Script/booking_process.js
+++ Script/booking_process.js
@@ -1,4 +1,10 @@
-let value = document.getElementById("getValueId").innerHTML = localStorage.getItem("check_in_data");
-let value12 = document.getElementById("getValueId").innerHTML = localStorage.getItem("check_out_data");
-let value123 = document.getElementById("getValueId").innerHTML = localStorage.getItem("adult_data");
-let value1234 = document.getElementById("getValueId").innerHTML = localStorage.getItem("children_data");
+//Get the value from 
+/*let check_in_data = localStorage.getItem("check_in_data");
+let check_out_data = localStorage.getItem("check_out_data");
+let adult_data = localStorage.getItem("adult_data");
+let children_data = localStorage.getItem("children_data");
+
+document.getElementById("value1").innerHTML += check_in_data;
+document.getElementById("value12").innerHTML += check_out_data;
+document.getElementById("value123").innerHTML += adult_data;
+document.getElementById("value1234").innerHTML += children_data;*/
