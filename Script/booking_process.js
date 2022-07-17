c8ce909fe6ab4d124e251fe2beafc7ebb9c6609d
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-07-17 21:19:28 +0530

    confirm

---
+++ Script/booking_process.js
@@ -0,0 +1,4 @@
+let value = document.getElementById("getValueId").innerHTML = localStorage.getItem("check_in_data");
+let value12 = document.getElementById("getValueId").innerHTML = localStorage.getItem("check_out_data");
+let value123 = document.getElementById("getValueId").innerHTML = localStorage.getItem("adult_data");
+let value1234 = document.getElementById("getValueId").innerHTML = localStorage.getItem("children_data");
--- Script/index_page_script.js
+++ Script/index_page_script.js
@@ -19,10 +19,16 @@ navBar.forEach(function (a) {
 
 
 function queryBooking() {
-    const check_in = document.getElementById("check_in").value;
-    const check_out = document.getElementById("check_out").value;
-    const adult = document.getElementById("adult").value;
-    const children = document.getElementById("children").value;
+    let check_in = document.getElementById("check_in").value;
+    let check_out = document.getElementById("check_out").value;
+    let adult = document.getElementById("adult").value;
+    let children = document.getElementById("children").value;
 
+    localStorage.setItem("check_in_data", check_in);
+    localStorage.setItem("check_out_data", check_out);
+    localStorage.setItem("adult_data", adult);
+    localStorage.setItem("children_data", children);
+
+    return false;
     
 }
